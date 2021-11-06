a23b62e8ebe5172aafc22872ce15b04c485ce7a5
GHOSTA1PHA1 <88077897+GHOSTA1PHA1@users.noreply.github.com>
2021-11-05 21:34:05 -0500

    jobs and job categories added for now

--- garrysmod/addons/darkrpmodification/lua/darkrp_customthings/jobs.lua
+++ garrysmod/addons/darkrpmodification/lua/darkrp_customthings/jobs.lua
@@ -15,7 +15,418 @@ https://darkrp.miraheze.org/wiki/DarkRP:CustomJobFields
 
 Add your custom jobs under the following line:
 ---------------------------------------------------------------------------]]
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+Team_Government = DarkRP.createJob("Judge", {
+    color = Color(69, 11, 235, 255),
+    model = {"models/player/breen.mdl"},
+    description = [[The Judge can put people in jail and also pardon them. This is a serious RP Role]],
+    weapons = {"ephone", "weapon_arc_atmcard", "med_kit", "weapon_deagle2", "arrest_stick", "unarrest_stick"},
+    command = "Judge",
+    max = 1,
+    salary = 500,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = false,
+    category = "Government",
+    NeedToChangeFrom = team_Civilian,
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(200)
+        ply:SetHealth(200)
+        ply:SetArmor(150)
+    end
+})
 
+Team_Civilian = DarkRP.createJob("Citizen", {
+    color = Color(176, 201, 19, 255),
+    model = {
+        "models/player/p2_chell.mdl",
+        "models/player/alyx.mdl",
+        "models/player/Group01/female_01.mdl",
+        "models/player/Group01/male_02.mdl",
+        "models/player/Group01/male_03.mdl",
+        "models/player/Group01/male_04.mdl",
+        "models/player/Group01/female_03.mdl"
+    },
+    description = [[Basic everyday person ]],
+    weapons = {"ephone", "weapon_arc_atmcard"},
+    command = "Civilian",
+    max = 0,
+    salary = 90,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = false,
+    category = "Civilian",
+    NeedToChangeFrom = team_Civilian,
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(125)
+        ply:SetHealth(125)
+        ply:SetArmor(150)
+    end
+})
+
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+Team_Government = DarkRP.createJob("Prosecutor", {
+    color = Color(69, 11, 235, 255),
+    model = {"models/player/kleiner.mdl"},
+    description = [[The Prosecutor is a lawyer who conducts the case against a defendant in a criminal court. This is a Serious RP role]],
+    weapons = {"ephone", "weapon_arc_atmcard"},
+    command = "Prosecutor",
+    max = 1,
+    salary = 250,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = false,
+    category = "Government",
+    NeedToChangeFrom = team_Civilian,
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(200)
+        ply:SetHealth(200)
+        ply:SetArmor(150)
+    end
+})
+
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+Team_Government = DarkRP.createJob("Defense Attorney ", {
+    color = Color(69, 11, 235, 255),
+    model = {"models/player/gman_high.mdl"},
+    description = [[The Defense Attorney is an attorney representing a defendant in a lawsuit or criminal prosecution.]],
+    weapons = {"ephone", "weapon_arc_atmcard"},
+    command = "Defense Attorney ",
+    max = 1,
+    salary = 250,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = false,
+    category = "Government",
+    NeedToChangeFrom = team_Civilian,
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(200)
+        ply:SetHealth(200)
+        ply:SetArmor(150)
+    end
+})
+
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+Team_Government = DarkRP.createJob("Mayor", {
+    color = Color(69, 11, 235, 255),
+    model = {"models/player/barney.mdl"},
+    description = [[The Mayor can set laws, put the city on lockdown, and set jail position.]],
+    weapons = {"ephone", "weapon_arc_atmcard", "weapon_deagle2"},
+    command = "Mayor",
+    max = 1,
+    salary = 300,
+    admin = 0,
+    vote = true,
+    hasLicense = true,
+    candemote = true,
+    category = "Government",
+    NeedToChangeFrom = team_Civilian,
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(200)
+        ply:SetHealth(200)
+        ply:SetArmor(150)
+    end
+})
+
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+Team_Government = DarkRP.createJob("Chief Of Police", {
+    color = Color(69, 11, 235, 255),
+    model = {"models/player/leet.mdl"},
+    description = [[The Chief of police is highest Rank in the police force, this is a serious Rp role]],
+    weapons = {"ephone", "weapon_arc_atmcard", "weapon_deagle2"},
+    command = "Chief of Police",
+    max = 1,
+    salary = 275,
+    admin = 0,
+    vote = true,
+    hasLicense = true,
+    candemote = true,
+    category = "Government",
+    NeedToChangeFrom = team_Civilian,
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(200)
+        ply:SetHealth(200)
+        ply:SetArmor(150)
+    end
+})
+
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+Team_City_Police = DarkRP.createJob("Police Officer", {
+    color = Color(69, 11, 235, 255),
+    model = {"models/player/dod_american.mdl"},
+    description = [[The Police Officer is sworn to protect and serve, this is a serious Rp role]],
+    weapons = {"ephone", "weapon_arc_atmcard", "weapon_glock2", "arrest_stick", "unarrest_stick", "stunstick"},
+    command = "Police Officer",
+    max = 10,
+    salary = 175,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = true,
+    category = "City Police",
+    NeedToChangeFrom = team_Civilian,
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(200)
+        ply:SetHealth(200)
+        ply:SetArmor(150)
+    end
+})
+
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+Team_City_Police = DarkRP.createJob("Detective", {
+    color = Color(69, 11, 235, 255),
+    model = {"models/player/dod_german.mdl"},
+    description = [[The Detective is a more advanced and strategic Officer of the law, this is a serious Rp role]],
+    weapons = {"ephone", "weapon_arc_atmcard", "weapon_glock2", "arrest_stick", "unarrest_stick", "stunstick", "weapon_pumpshotgun2", "lockpick"},
+    command = "Detective",
+    max = 4,
+    salary = 200,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = true,
+    category = "City Police",
+    NeedToChangeFrom = team_Civilian,
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(200)
+        ply:SetHealth(200)
+        ply:SetArmor(150)
+    end
+})
+
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+Team_City_Police = DarkRP.createJob("Colonel", {
+    color = Color(69, 11, 235, 255),
+    model = {"models/player/urban.mdl"},
+    description = [[The Colonel is an advisor for the Chief of Police, This is a serious RP role]],
+    weapons = {"ephone", "weapon_arc_atmcard", "weapon_glock2", "arrest_stick", "unarrest_stick", "stunstick", "weapon_pumpshotgun2", "lockpick"},
+    command = "Colonel",
+    max = 1,
+    salary = 200,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = true,
+    category = "City Police",
+    NeedToChangeFrom = team_Civilian,
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(200)
+        ply:SetHealth(200)
+        ply:SetArmor(150)
+    end
+})
+
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+Team_First_Responder = DarkRP.createJob("EMT", {
+    color = Color(230, 39, 152, 255),
+    model = {
+        "models/player/Group03m/female_06.mdl",
+        "models/player/Group03m/male_02.mdl",
+        "models/player/Group03m/male_03.mdl"
+    },
+    description = [[The EMT is the lowest ranking Paramedic , can heal other players]],
+    weapons = {"ephone", "weapon_arc_atmcard", "med_kit"},
+    command = "EMT",
+    max = 4,
+    salary = 200,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = false,
+    category = "First Responder",
+    NeedToChangeFrom = team_Civilian,
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(200)
+        ply:SetHealth(200)
+        ply:SetArmor(150)
+    end
+})
+
+--]]
+Team_First_Responder = DarkRP.createJob("Doctor", {
+    color = Color(230, 39, 152, 255),
+    model = {
+        "models/player/mossman_arctic.mdl",
+        "models/player/odessa.mdl"
+    },
+    description = [[The Doctor can heal other players]],
+    weapons = {"ephone", "weapon_arc_atmcard", "med_kit"},
+    command = "Doctor",
+    max = 2,
+    salary = 220,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = false,
+    category = "First Responder",
+    NeedToChangeFrom = team_Civilian,
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(200)
+        ply:SetHealth(200)
+        ply:SetArmor(150)
+    end
+})
+
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+Team_Civilian = DarkRP.createJob("Gun Dealer", {
+    color = Color(54, 227, 11, 255),
+    model = {"models/player/monk.mdl"},
+    description = [[The Gun Dealer deals guns duh! Don't get caught selling illegal weapons though!]],
+    weapons = {"ephone", "weapon_arc_atmcard"},
+    command = "Gun Dealer",
+    max = 5,
+    salary = 150,
+    admin = 0,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    category = "Civilian",
+    NeedToChangeFrom = team_Civilian,
+    PlayerSpawn = function(ply)
+        ply:SetMaxHealth(200)
+        ply:SetHealth(200)
+        ply:SetArmor(150)
+    end
+})
+
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+TEAM_Criminal = DarkRP.createJob("Drug Dealer", {
+    color = Color(20, 18, 18, 255),
+    model = {"models/player/soldier_stripped.mdl"},
+    description = [[Deal some drugs yo! just don't get caught!]],
+    weapons = {"ephone", "weapon_arc_atmcard"},
+    command = "Drug Dealer",
+    max = 4,
+    salary = 200,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = false,
+    category = "Criminal",
+    NeedToChangeFrom = TEAM_Civilian
+})
+
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+TEAM_Criminal = DarkRP.createJob("Thief ", {
+    color = Color(20, 18, 18, 255),
+    model = {"models/player/arctic.mdl"},
+    description = [[Steal some shit, don't get caught!]],
+    weapons = {"ephone", "weapon_arc_atmcard", "lockpick"},
+    command = "Thief",
+    max = 4,
+    salary = 100,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = false,
+    category = "Criminal",
+    NeedToChangeFrom = TEAM_Civilian
+})
+
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+TEAM_Hatchet_Family = DarkRP.createJob("Hatchet Family Guard", {
+    color = Color(111, 20, 201, 255),
+    model = {
+        "models/player/Group01/male_04.mdl",
+        "models/player/Group01/male_03.mdl",
+        "models/player/Group01/female_05.mdl",
+        "models/player/Group01/female_06.mdl"
+    },
+    description = [[Hatchet Family Guard, keep the Hatchet Family safe and work your way through the ranks!]],
+    weapons = {"ephone", "weapon_arc_atmcard", "lockpick", "stunstick"},
+    command = "Guard",
+    max = 6,
+    salary = 100,
+    admin = 0,
+    vote = false,
+    hasLicense = false,
+    candemote = false,
+    category = "Hatchet Family",
+    NeedToChangeFrom = TEAM_Civilian
+})
+
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+TEAM_Hatchet_Family = DarkRP.createJob("Tony Hatchet", {
+    color = Color(111, 20, 201, 255),
+    model = {"models/player/phoenix.mdl"},
+    description = [[One of the Hatchet Family Leaders]],
+    weapons = {"ephone", "weapon_arc_atmcard", "lockpick", "stunstick", "weapon_deagle2", "weapon_ak472"},
+    command = "Tony Hatchet",
+    max = 1,
+    salary = 500,
+    admin = 1,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    category = "Hatchet Family",
+    NeedToChangeFrom = TEAM_Civilian
+})
+
+--[[
+    Generated using: DarkRP | Job Generator
+    https://csite.io/tools/gmod-darkrp-job
+--]]
+TEAM_Hatchet_Family = DarkRP.createJob("Garry Hatchet", {
+    color = Color(111, 20, 201, 255),
+    model = {"models/player/phoenix.mdl"},
+    description = [[One of the Hatchet Family Leaders]],
+    weapons = {"ephone", "weapon_arc_atmcard", "lockpick", "stunstick", "weapon_deagle2", "weapon_ak472"},
+    command = "Garry Hatchet",
+    max = 1,
+    salary = 500,
+    admin = 1,
+    vote = false,
+    hasLicense = true,
+    candemote = false,
+    category = "Hatchet Family",
+    NeedToChangeFrom = TEAM_Civilian
+})
 
 
 --[[---------------------------------------------------------------------------
